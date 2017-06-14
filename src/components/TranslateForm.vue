5e5640e384d42fa4524abfa8772202590e936c7a
Andrew Salcedo <asalcedo07@gmail.com>
2017-06-13 22:39:21 -0400

    add <form> and data() to TranslateForm

--- src/components/TranslateForm.vue
+++ src/components/TranslateForm.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="translateForm">
-        hello world
+    <form v-on:submit="formSubmit">
+        <input type="text" v-model="textToTranslate" placeholder="Enter a word">
+        <input type="submit" value="Translate">
+    </form>
   </div>
 </template>
 
 <script>
 export default {
   name: 'translateForm', 
+  data() {
+      return {
+          textToTranslate: ''
+      }
+  }
 }
 </script>
 
